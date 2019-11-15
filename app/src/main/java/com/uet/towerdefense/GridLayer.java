eba05a8c86b59eecc3146a1f384df8cd390f64d1
Nguyen Chien <nguyenchien19122000@gmail.com>
2019-11-15 13:03:41 +0700

    oh, sorry,commit 16th wrong, complete the move and tower place holder

--- app/src/main/java/com/uet/towerdefense/GridLayer.java
+++ app/src/main/java/com/uet/towerdefense/GridLayer.java
@@ -122,72 +122,9 @@ public class GridLayer {
 
     if(enemy.getY()>=38.19*per100Height-2&&enemy.getY()<=38.19*per100Height+2)
     {
-
         return EnemyDirection.RIGHT;
-
     }
-//    if (enemy.getX() > per100Width * 19 && enemy.getY() < per100Height * 38) {
-//      if (direction == EnemyDirection.RIGHT) {
-//        return EnemyDirection.DOWN;
-//      }
-//    }
     return direction;
   }
 
-  @Deprecated
-  public void moveEnemy(Enemy enemy) {
-
-    moveRightTilPosition(enemy, 0.1828);
-    moveDownTilPosition(enemy, 0.1828, 0.1829, 0.8055);
-
-    if (enemy.getY() >= 0.8055 * SCREEN_WIDTH - 2 && enemy.getY() <= 0.8056 * SCREEN_WIDTH + 2) {
-      moveRightTilPosition(enemy, 0.332);
-    }
-
-    moveUpTilPosition(enemy);
-
-    if (enemy.getY() >= 0.0694 * SCREEN_WIDTH - 2 && enemy.getY() <= 0.0695 * SCREEN_WIDTH + 2) {
-      moveRightTilPosition(enemy, 0.8476);
-    }
-
-    moveDownTilPosition(enemy, 0.8476, 0.8476, 0.3819);
-
-    moveRightTilPosition(enemy);
-
-    if (enemy.getX() >= SCREEN_HEIGHT - 1) {
-      return;
-    }
-
-    handler.postDelayed(() -> moveEnemy(enemy), 20);
-  }
-
-  private void moveRightTilPosition(Enemy enemy) {
-    if (enemy.getY() >= 0.3819 * SCREEN_WIDTH - 2 && enemy.getY() <= 0.3819 * SCREEN_WIDTH + 2) {
-      if (enemy.getX() <= SCREEN_HEIGHT && enemy.getX() >= 0.8476 * SCREEN_HEIGHT) {
-        enemy.moveRight();
-      }
-    }
-  }
-
-  private void moveUpTilPosition(Enemy enemy) {
-    if (enemy.getX() >= 0.332 * SCREEN_HEIGHT - 2 && enemy.getX() <= 0.333 * SCREEN_HEIGHT + 2) {
-      if (enemy.getY() > 0.0694 * SCREEN_WIDTH) {
-        enemy.moveUp();
-      }
-    }
-  }
-
-  private void moveDownTilPosition(Enemy enemy, double v, double v2, double v3) {
-    if (enemy.getX() >= v * SCREEN_HEIGHT - 2 && enemy.getX() <= v2 * SCREEN_HEIGHT + 2) {
-      if (enemy.getY() < v3 * SCREEN_WIDTH) {
-        enemy.moveDown();
-      }
-    }
-  }
-
-  private void moveRightTilPosition(Enemy enemy, double v) {
-    if (enemy.getX() < v * SCREEN_HEIGHT) {
-      enemy.moveRight();
-    }
-  }
 }
